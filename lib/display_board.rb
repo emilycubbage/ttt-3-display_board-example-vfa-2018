da7aa59914468a05622d667e34997c84325944ae
Emily Cubbage <emilycubbage@gmail.com>
2018-06-22 02:34:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 
 def display_board
   cell = "   "
-  puts [cell | cell | cell]
+  puts [cell, cell, cell]
   puts "-----------"
-  puts [cell,"|",cell,"|",cell]
+  puts [cell,cell,cell]
   puts "-----------"
-  puts [cell,"|",cell,"|",cell]
+  puts [cell,cell,cell]
 end
